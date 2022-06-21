2b6cbc994a847ccc2a801e5dd6af9aa6d4a1e656
Michael Maroszek <maroszek@gmx.net>
2022-06-21 11:01:51 +0200

    WIP

--- Prometheus/module.php
+++ Prometheus/module.php
@@ -11,14 +11,48 @@ class Prometheus extends WebHookModule
         parent::__construct($InstanceID, 'metrics');
     }
 
+    // Source: https://stackoverflow.com/a/4356295/10288655
+    private function generateRandomString($length) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    
     public function Create()
     {
 
         //Never delete this line!
         parent::Create();
+        
+        // Hook
+        $this->RegisterPropertyBoolean('HookEnabled', true);
+        $this->RegisterPropertyString('Username', IPS_GetLicensee()); // Missing Hook prefix due to backwards compatibility
+        $this->RegisterPropertyString('Password', $this->generateRandomString(16)); // Missing Hook prefix due to backwards compatibility
+        
+        // Push
+        $this->RegisterPropertyBoolean('PushEnabled', false);
+        $this->RegisterPropertyString('PushEndpointURL', 'https://prometheus-prod-01-eu-west-0.grafana.net/api/prom/push');
+        $this->RegisterPropertyString('PushEndpointUsername', '467358');
+        $this->RegisterPropertyString('PushEndpointPassword', '');
+        $this->RegisterPropertyInteger('PushInterval', 10);
+        
+        $this->RegisterTimer("Push", 0, "PMM_Push(\$_IPS['TARGET']);");
+    }
 
-        $this->RegisterPropertyString('Username', '');
-        $this->RegisterPropertyString('Password', '');
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+        
+        if ($this->ReadPropertyBoolean("Push")) {
+            $this->SetTimerInterval("Push", $this->ReadPropertyInteger('PushInterval') * 1000);
+        }
+        else {
+            $this->SetTimerInterval("Push", 0);
+        }
     }
 
     /**
@@ -30,7 +64,13 @@ class Prometheus extends WebHookModule
         //Never delete this line!
         parent::ProcessHookData();
 
-        if ((IPS_GetProperty($this->InstanceID, 'Username') != '') || (IPS_GetProperty($this->InstanceID, 'Password') != '')) {
+        if (!$this->ReadPropertyBoolean('HookEnabled')) {
+            header('HTTP/1.0 403 Forbidden');
+            echo 'Forbidden';
+            return;
+        }
+        
+        if ($this->ReadPropertyString('Username') || $this->ReadPropertyString('Password')) {
             if (!isset($_SERVER['PHP_AUTH_USER'])) {
                 $_SERVER['PHP_AUTH_USER'] = '';
             }
@@ -38,7 +78,7 @@ class Prometheus extends WebHookModule
                 $_SERVER['PHP_AUTH_PW'] = '';
             }
 
-            if (($_SERVER['PHP_AUTH_USER'] != IPS_GetProperty($this->InstanceID, 'Username')) || ($_SERVER['PHP_AUTH_PW'] != IPS_GetProperty($this->InstanceID, 'Password'))) {
+            if (($_SERVER['PHP_AUTH_USER'] != $this->ReadPropertyString('Username')) || ($_SERVER['PHP_AUTH_PW'] != $this->ReadPropertyString('Password'))) {
                 header('WWW-Authenticate: Basic Realm="Prometheus WebHook"');
                 header('HTTP/1.0 401 Unauthorized');
                 echo 'Authorization required';
@@ -47,5 +87,33 @@ class Prometheus extends WebHookModule
         }
 
         include __DIR__ . '/metrics.php';
+    }
+    
+    public function Push() {
+        
+        ob_start();
+        include __DIR__ . '/metrics.php';
+        $data = ob_get_contents();
+        ob_end_clean();
+        
+        $authentication = base64_encode($this->ReadPropertyString('PushEndpointUsername') . ":" . $this->ReadPropertyString('PushEndpointPassword'));
+        
+        // Wir müssen die Daten noch passend "verpacken"
+        // https://prometheus.io/docs/prometheus/latest/storage/#remote-storage-integrations
+        // The read and write protocols both use a snappy-compressed protocol buffer encoding over HTTP
+        // https://github.com/prometheus/prometheus/blob/main/prompb/remote.proto
+        
+        $options = [
+            'http' => [
+                'header'        => "Authorization: Basic " . $authentication . "\r\nContent-Type: application/json\r\n",
+                'method'        => 'POST',
+                'content'       => $data,
+                'ignore_errors' => true
+            ]
+        ];
+        
+        $context = stream_context_create($options);
+        
+        echo file_get_contents($this->ReadPropertyString('PushEndpointURL'), false, $context);
     }
 }
